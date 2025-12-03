ccb3c9e4bae29b2b2c9ecdfa1f2fb0d83d62781d
michaelnardelli <michael.nardelli@marconirovereto.it>
2025-12-03 15:09:40 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    isrMemoryCacheSize: 0,
-  },
 };
 
 export default nextConfig;
